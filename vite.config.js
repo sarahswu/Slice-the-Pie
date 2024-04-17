20aa72edf6266d75dc6f99c07c25caf3c06a3687
sarahswu <56704956+sarahswu@users.noreply.github.com>
2024-04-16 20:22:05 -0700

    deploy github pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
  * @type { import('vite').UserConfig }
  */
 export default {
+  base: "/Slice-the-Pie/",
   plugins: [reactRefresh()],
   server: {
     hmr: {
